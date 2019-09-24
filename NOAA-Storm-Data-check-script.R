f0d4df7858876c824e30757dbfff0174569f0bc7
Antonio Rubiera <rubiera@users.noreply.github.com>
2019-09-24 13:56:27 -0400

    minor updates

--- NOAA-Storm-Data-check-script.R
+++ NOAA-Storm-Data-check-script.R
@@ -1,5 +1,6 @@
 library(data.table)
 library(tidyverse)
+library(rsconnect)
 
 StormData <- read.csv("./data/StormData.csv")
 StormDataDT <- fread("./data/StormData.csv")
@@ -14,4 +15,13 @@ colnames(StormDataDT)
 head(StormData)
 head(StormDataTB)
 head(StormDataDT)
+
+#straw for rpubs
+rpubsUpload("NOAA Storm Data Analysis",
+            "./_site/index.html","https://rpubs.com/mulchlax")
+#update for image
+rpubsUpload("NOAA Storm Data Analysis",
+      "./_site/index_files/figure-html/pressure-1.png",
+      "http://rpubs.com/mulchlax/531829")
+
 
