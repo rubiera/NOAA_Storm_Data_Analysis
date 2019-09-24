dcaec6eb4f37e5cd32a01e0158b1515e24d6c91a
Antonio Rubiera <rubiera@users.noreply.github.com>
2019-09-24 13:19:57 -0400

    Test script

--- NOAA-Storm-Data-check-script.R
+++ NOAA-Storm-Data-check-script.R
@@ -1,10 +1,7 @@
 library(data.table)
 library(tidyverse)
 
-
-StormData <- read.csv("./data/repdata_data_StormData.csv.bz2")
-head(StormData)
-write.csv(StormData, file="./data/StormData.csv")
+StormData <- read.csv("./data/StormData.csv")
 StormDataDT <- fread("./data/StormData.csv")
 StormDataTB <- as_tibble(StormData) 
 
